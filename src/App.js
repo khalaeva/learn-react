6c617855b5022358bf941eb523f101180f226521
khalaeva <halaevaanna@gmail.com>
2023-11-17 15:16:53 +0300

    hw state onClick

--- src/App.js
+++ src/App.js
@@ -2,20 +2,14 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [showText, setShowText] = useState(true)
-  const [textColor, setTextColor] = useState("red")
+  const [count, setCount] = useState(0)
   
   return (
     <div className='App'>
-      <button 
-        onClick={() => {
-          setTextColor((textColor === "red") ? "green" : "red")
-          setShowText(!showText)
-        }}
-      >
-        Show/Hide
-      </button>
-      {showText && <h1 style={{color: textColor}}>Hello World</h1>}
+      {count}
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+      <button onClick={() => setCount(0)}>Set to Zero</button>
     </div>
   )
 }
