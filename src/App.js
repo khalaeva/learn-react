83b129b36797697fe94a63a64a9611d3c5253936
khalaeva <halaevaanna@gmail.com>
2023-11-17 14:37:13 +0300

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const age = 15
+  const isGreen = false
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="App">
+    {age >= 18 ? <h1>Over Age</h1> : <h1>Under Age</h1>}
+    <h1 style={{color: isGreen ? "green" : "red"}}>This has Color</h1>
+    
+    {isGreen && <button>Button</button>}
+    
+    <User name="Anna" age={21}/>
+    <User name="Pedro" age={22}/>
+  </div>
+  );
+}
+
+const User = (props) => {
+  return (
+    <div className='name'>
+      <h1>{props.name}</h1>
+      <h1>{props.age}</h1>
     </div>
   );
 }
+
+// const getName = () => { - js func
+//   return "Anna"
+// }
+
+// const GetNameComponent = () => { - react comp
+//   return <h1>Anna</h1>
+// }
+
 
 export default App;
