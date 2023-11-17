a13bd6231cdbab06aedc2e27336c8da29847f02b
khalaeva <halaevaanna@gmail.com>
2023-11-17 15:00:25 +0300

    useState hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import './App.css';
-import { Planet } from './Planet';
-function App() {
-  const planets = [
-    { name: "Mars", isGasPlanet: false },
-    { name: "Earth", isGasPlanet: false },
-    { name: "Jupiter", isGasPlanet: true },
-    { name: "Venus", isGasPlanet: false },
-    { name: "Neptune", isGasPlanet: true },
-    { name: "Uranus", isGasPlanet: true },
-  ];
+import { useState } from 'react';
 
+function App() {
+  const [age, setAge] = useState(0)
+
+  const increaseAge = () => {
+    setAge(age + 1)
+  }
   return (
     <div className='App'>
-      {planets.map((planet, i) => {
-        return <Planet name={planet.name} isGasPlanet={planet.isGasPlanet}/>
-      })}
+      {age}
+      <button onClick={increaseAge}>
+        Increase Age
+      </button>
     </div>
   )
 }
-
-
 
 export default App;
--- src/Planet.js
+++
@@ -1,7 +0,0 @@
-export const Planet = (props) => {
-    return (
-        <div className="name">
-            {props.isGasPlanet && <h1>{props.name}</h1>}
-        </div>
-    )
-}
