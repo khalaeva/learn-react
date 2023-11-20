489adc4181189f8ca6b8f46d5fc209a91273d851
Khalaeva <halaevaanna@gmail.com>
2023-11-20 17:03:19 +0300

    useEffect

--- src/Text.js
+++ src/Text.js
@@ -1,7 +1,11 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export const Text = () => {
     const [text, setText] = useState("")
+
+    useEffect(() => {
+      console.log('MOUNTED')
+    }, [])
 
     return (
         <div>
