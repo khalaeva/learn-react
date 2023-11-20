cf1d8f208a718977ca585965e6dc11292aa660a5
Khalaeva <halaevaanna@gmail.com>
2023-11-20 16:13:19 +0300

    delete task

--- src/App.js
+++ src/App.js
@@ -10,8 +10,16 @@ function App() {
   }
 
   const addTask = () => {
-    setTodoList([...todoList, newTask])
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask
+    }
+    setTodoList([...todoList, task])
   }
+
+  const deleteTask = (id) => {
+    setTodoList(todoList.filter((task) => task.id !== id))
+  } 
 
   return (
     <div className='App'>
@@ -21,7 +29,11 @@ function App() {
       </div>
       <div className='list'>
         {todoList.map((task, i) => {
-          return <h3 key={i}>{task}</h3>
+          return (
+            <div className="task">
+              <h3 key={i}>{task.taskName}</h3>
+              <button onClick={() => deleteTask(task.id)}>X</button>
+            </div>)
         })}
       </div>
     </div>
