7eccc2fdc14dee2ab4f51c291103c371ed846791
Khalaeva <halaevaanna@gmail.com>
2023-11-21 15:56:18 +0300

    creating-routes

--- src/App.js
+++ src/App.js
@@ -1,52 +1,22 @@
-import './App.css';
-import { useState } from 'react';
-import { Task } from './Task';
+import './App.css'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { Home } from './pages/Home';
+import { Menu } from './pages/Menu';
+import { Contact } from './pages/Contact';
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
-  const [newTask, setNewTask] = useState("")
-
-  const handleChange = (event) => {
-    setNewTask(event.target.value)
-  }
-
-  const addTask = () => {
-    const task = {
-      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
-      taskName: newTask,
-      completed: false
-    }
-    setTodoList([...todoList, task])
-  }
-
-  const deleteTask = (id) => {
-    setTodoList(todoList.filter((task) => task.id !== id))
-  } 
-
-  const completeTask = (id) => {
-    setTodoList(
-      todoList.map((task) => {
-        if (task.id === id) {
-          return {...task, completed: true}
-        } else {
-          return task
-        }
-      })
-    )
-  }
   return (
     <div className='App'>
-      <div className='addTask'>
-        <input onChange={handleChange}/>
-        <button onClick={addTask}>Add Task</button>
-      </div>
-      <div className='list'>
-        {todoList.map((task) => {
-          return <Task taskName={task.taskName} id={task.id} completed={task.completed} deleteTask={deleteTask} completeTask={completeTask}/>
-        })}
-      </div>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/menu' element={<Menu/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+          <Route path='*' element={<h1>Page not found</h1>}/>
+        </Routes>
+      </Router>
     </div>
-  )
+  ) 
 }
 
 export default App;
