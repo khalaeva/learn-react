8c6014e1a181e5de6b9ab34f468fcd0a24fc96e3
khalaeva <halaevaanna@gmail.com>
2023-11-23 11:33:49 +0300

    useContext

--- src/pages/Menu.js
+++
@@ -1,3 +0,0 @@
-export const Menu = () => {
-    return <h1>This Is The Menu</h1>
-}
---
+++ src/pages/Profile.js
@@ -0,0 +1,13 @@
+import { ChangeProfile } from "../components/ChangeProfile"
+import { useContext } from 'react'
+import { AppContext } from '../App'
+
+export const Profile = () => {
+  const { username } = useContext(AppContext)
+  return (
+    <div>
+        <h1>Profile {username}</h1>
+        <ChangeProfile/>
+    </div>
+  )
+}
