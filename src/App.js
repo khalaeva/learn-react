2f775f8603cced8848f075f90c27344a8ad26d21
khalaeva <halaevaanna@gmail.com>
2023-11-23 13:20:36 +0300

    form

--- src/App.js
+++ src/App.js
@@ -1,36 +1,10 @@
 import './App.css'
-import { useState, createContext } from 'react'
-import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { Home } from './pages/Home';
-import { Profile } from './pages/Profile';
-import { Contact } from './pages/Contact';
-import { Navbar } from './Navbar';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
-export const AppContext = createContext()
+import { Form } from './components/Form';
 
 function App() {
-  const client = new QueryClient({defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false
-    }
-  }})
-  const [username, setUsername] = useState('Anna')
   return (
     <div className='App'>
-      <QueryClientProvider client={client}>
-      <AppContext.Provider value={{username, setUsername}}>
-      <Router>
-        <Navbar/>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/profile' element={<Profile/>}/>
-          <Route path='/contact' element={<Contact/>}/>
-          <Route path='*' element={<h1>Page not found</h1>}/>
-        </Routes>
-      </Router>
-      </AppContext.Provider>
-      </QueryClientProvider>
+      <Form/>
     </div>
   ) 
 }
