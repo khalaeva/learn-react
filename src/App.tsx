90ae3a809ff6c1a8eb7f4fb22c279dcb10f0f81c
khalaeva <halaevaanna@gmail.com>
2023-11-24 12:39:01 +0300

    store react

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
-import './App.css';
-import { Person, Country } from './Person';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Contact } from "./pages/Contact";
+import { Login } from "./pages/Login";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 function App() {
-
-  // const getAge = (name: string): number => {
-  //   return 22
-  // }
-
   return (
     <div className="App">
-        <Person
-          name="Anna"
-          email="asdsa"
-          age={22}
-          isMarried={false}
-          friends={['sdfs', 'sdfsf', 'fgdfgd']}
-          country={Country.Russia}
-        />
+      <Provider store={store}>
+        <Router>
+          <Link to="/"> Home </Link>
+          <Link to="/login"> Login </Link>
+          <Link to="/contact"> Contact </Link>
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Router>
+      </Provider>
     </div>
   );
 }
